d8fa016e8ecd1b85d62e4f2c71320ea874dd21f6
devopspark <102714549+devopspark@users.noreply.github.com>
2024-01-25 16:18:50 +0900

    Update news.py

--- news.py
+++ news.py
@@ -3,14 +3,19 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+import time
 
-driver = webdriver.Chrome()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+driver = webdriver.Chrome(options = options)
 driver.get("https://search.naver.com/search.naver?where=news&sm=tab_jum&query=%EC%86%8D%EB%B3%B4")
+time.sleep(1)
 titleList = []
 
 for i in range(20):
     body = driver.find_element(By.CSS_SELECTOR, "body")
     body.send_keys(Keys.END)
+    time.sleep(1)
 
 soup = bs(driver.page_source,"lxml")
 title = soup.select("a.news_tit")
